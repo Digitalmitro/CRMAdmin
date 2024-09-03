01351bd9eea0dd4bc0a0616693125fab0ec9548f
sayaksengupta <sayak.sengupta@atpl.live>
2024-09-04 01:22:50 +0530

    admin edit and delete employee temp delete

--- src/Components/Pages/Employee/EmployeeActivity.jsx
+++ src/Components/Pages/Employee/EmployeeActivity.jsx
@@ -435,7 +435,9 @@ const handleDel = (id) => {
                           >
                             View
                           </button>
-                          <button
+                          {/* TEMP REMOVAL OF ADMIN EDIT AND DELETE ACTIONS */}
+
+                          {/* <button
                             className="btn btn-dark ms-2"
                             onClick={() =>
                               navigate(`/updateemployee/${res._id}`)
@@ -480,8 +482,8 @@ const handleDel = (id) => {
                                 </g>
                               </g>
                             </svg>
-                          </button>
-                          <button
+                          </button> */}
+                          {/* <button
                             className="btn btn-danger ms-2"
                             onClick={() => handleDel(res._id)}
                           >
@@ -496,7 +498,9 @@ const handleDel = (id) => {
                               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                               <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                             </svg>
-                          </button>
+                          </button> */}
+
+                          {/* TEMP REMOVAL OF ADMIN EDIT AND DELETE ACTIONS */}
                         </td>
                       </tr>
                     </>
