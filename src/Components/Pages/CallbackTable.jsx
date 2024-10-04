75f05eae4d398868106042c078c724a05870c212
sayaksengupta <sayak.sengupta@atpl.live>
2024-10-05 03:29:32 +0530

    callback date

--- src/Components/Pages/CallbackTable.jsx
+++ src/Components/Pages/CallbackTable.jsx
@@ -489,7 +489,7 @@ const CallbackTable = () => {
                         <td>{res?.name}</td>
                         <td>{res?.email}</td>
                         <td>{res?.phone}</td>
-                        <td>{res?.calldate}</td>
+                        <td>{moment(res?.calldate).format("DD/MM/YYYY")}</td>
                         <td>{res?.domainName}</td>
                         <td>{res?.address}</td>
                         <td>{res?.comments}</td>
